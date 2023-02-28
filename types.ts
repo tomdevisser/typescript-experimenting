38408bc8988e4173c9f1798828f8cf9886fb7cb5
Thomas de Visser <thomasdevisser@pm.me>
2023-02-28 14:34:16 +0100

    Added more types

--- types.ts
+++ types.ts
@@ -1,7 +1,12 @@
 const thisIsPi: number = 3.14;
 const movieTitle: string = "The Godfather";
+const isRaining: boolean = false;
 
 movieTitle = 9; // Error: Type 'number' is not assignable to type 'string'.
-thisIsPi = "3.14"; // Error: Type 'string' is not assignable to type 'number'.
 
+thisIsPi = "3.14"; // Error: Type 'string' is not assignable to type 'number'.
 thisIsPi.toUpperCase(); // Error: Property 'toUpperCase' does not exist on type 'number'.
+
+let type: any = "string";
+type = 9; // This is valid
+type = true; // This is valid
