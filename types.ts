977b08bd790ec14cac63288d08fa086782bcfce6
Thomas de Visser <thomasdevisser@pm.me>
2023-02-28 14:43:16 +0100

    Typing functions

--- types.ts
+++ types.ts
@@ -8,5 +8,12 @@ thisIsPi = "3.14"; // Error: Type 'string' is not assignable to type 'number'.
 thisIsPi.toUpperCase(); // Error: Property 'toUpperCase' does not exist on type 'number'.
 
 let type: any = "string";
-type = 9; // This is valid
-type = true; // This is valid
+type = 9; // This is valid.
+type = true; // This is valid.
+
+let futureBoss: string; // This fixed delayed initialization errors.
+
+// Typing functions in TypeScript.
+function square(x: number): number {
+  return x * x;
+}
